fb53e3b6dda6b6deb7e38d9bcc1264f90f4d4f11
강찬석 <ssimjunlke@naver.com>
2022-01-12 00:10:25 +0900

    UserListComponent , Delete버튼 생성

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          안녕하세요 test입니다
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          찬석이의 React 도전기
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;

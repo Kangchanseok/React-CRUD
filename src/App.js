964a4c0e061b1b7dc37457f37df09f02cd44e49f
강찬석 <ssimjunlke@naver.com>
2022-01-13 00:46:10 +0900

    add/edit/route 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import AppRouter from './component/route/RouterComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요 test입니다
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          찬석이의 React 도전기
-        </a>
-      </header>
+    <div>
+      <AppRouter />
     </div>
   );
 }
